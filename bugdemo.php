40a2eaa4b36e64704fbb60c27326aae18b67b724
Dimitar Dinchev <dinchev.dimitar@gmail.com>
2018-01-04 14:41:54 +0200

    Auto detect line endings.

--- bugdemo.php
+++ bugdemo.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!ini_get("auto_detect_line_endings")) {
+    ini_set("auto_detect_line_endings", '1');
+}
+
 require __DIR__ . '/vendor/autoload.php';
 
 $csv = \League\Csv\Reader::createFromPath(__DIR__ . '/test.csv', 'r');
